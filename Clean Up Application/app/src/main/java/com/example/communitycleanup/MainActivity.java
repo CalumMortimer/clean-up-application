11e6350452fa7ec621bab17984aa1404fe5931c2
Calum Mortimer SD2019 <ekb19183@muir-09.cis.strath.ac.uk>
2020-03-04 13:06:05 +0000

    Login Method Added and Linked to Login Button

--- Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
+++ Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.communitycleanup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void logInAttempt(View view)
+    {
+        EditText userNameBox = (EditText)findViewById(R.id.editText);
+        EditText passWordBox = (EditText)findViewById(R.id.editText3);
 
+        if ((userNameBox.getText().toString()=="admin")&&(passWordBox.getText().toString()=="password"))
+        {
+            //user is authenticated
+        }
+        else
+        {
+            //user is not authenticated
 
+        }
+    }
 
 }
