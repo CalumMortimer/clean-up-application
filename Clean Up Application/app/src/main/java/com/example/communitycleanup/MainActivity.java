4326a83f4cba45a1794e8ce4bbaaf1ef0cfd4efe
Calum Mortimer SD2019 <ekb19183@muir-09.cis.strath.ac.uk>
2020-03-04 15:37:20 +0000

    Passed User Data to the Homescreen Activity

--- Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
+++ Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.communitycleanup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -17,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
     {
         EditText userNameBox = (EditText)findViewById(R.id.editText);
         EditText passWordBox = (EditText)findViewById(R.id.editText3);
+        String username = userNameBox.getText().toString();
+        String password = passWordBox.getText().toString();
 
-        if ((userNameBox.getText().toString()=="admin")&&(passWordBox.getText().toString()=="password"))
+        if ((username.equals("admin"))&&(password.equals("password")))
         {
             //user is authenticated
+            User authenticatedUser = new User();
+            authenticatedUser.setUsername(username);
+            authenticatedUser.setPassword(password);
+
+            Intent intent = new Intent(this,HomeScreen.class);
+            String mySerial = authenticatedUser.getSerialData();
+            intent.putExtra("authenticated_user",mySerial);
+
+
+
+       //     startActivity(intent);
+            startActivity(intent);
         }
         else
         {
