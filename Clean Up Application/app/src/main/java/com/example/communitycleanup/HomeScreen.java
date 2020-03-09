00abe1f9de1f1399db6e300de9b50878912b362f
Calum Mortimer SD2019 <ekb19183@kelvin-16.cis.strath.ac.uk>
2020-03-09 12:42:44 +0000

    Button Grid Created for Main Menu

--- Clean Up Application/app/src/main/java/com/example/communitycleanup/HomeScreen.java
+++ Clean Up Application/app/src/main/java/com/example/communitycleanup/HomeScreen.java
@@ -3,6 +3,7 @@ package com.example.communitycleanup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
     User authenticatedUser = new User();
@@ -11,16 +12,32 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Intent intent = getIntent();
+        /*Intent intent = getIntent();
         String myUserData = intent.getStringExtra("authenticated_user");
         User myUser = new User(myUserData);
-        authenticatedUser = myUser;
+        authenticatedUser = myUser;*/
 
-        System.out.println(authenticatedUser.getUsername());
+       /* System.out.println(authenticatedUser.getUsername());
         System.out.println(authenticatedUser.getPassword());
         System.out.println(authenticatedUser.getEmail());
         System.out.println(authenticatedUser.getFirstName());
         System.out.println(authenticatedUser.getLastName());
-        System.out.println(authenticatedUser.getCommunityGroup());
+        System.out.println(authenticatedUser.getCommunityGroup());*/
+    }
+
+    public void myMethod(View view)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+
+
+        User myUser = new User();
+
+        String myString = myUser.getSerialData();
+
+       // System.out.println(myString);
+
+        intent.putExtra("my data",myString);
+
+        startActivity(intent);
     }
 }
--- Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
+++ Clean Up Application/app/src/main/java/com/example/communitycleanup/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
             String mySerial = authenticatedUser.getSerialData();
             intent.putExtra("authenticated_user",mySerial);
 
-
-
        //     startActivity(intent);
             startActivity(intent);
         }
