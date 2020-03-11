a4e6180cb975309426b1120cd3e17351a6ef4a81
Calum Mortimer SD2019 <ekb19183@muir-11.cis.strath.ac.uk>
2020-03-11 14:22:27 +0000

    EventDatabase Data Added

--- Clean-Up-Application/app/src/main/java/com/example/communitycleanup/EventDatabase.java
+++ Clean-Up-Application/app/src/main/java/com/example/communitycleanup/EventDatabase.java
@@ -16,7 +16,7 @@ public class EventDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS EVENTS(E_ID INTEGER PRIMARY KEY AUTOINCREMENT,DESCRIPTION VARCHAR(320) NOT NULL,LOCATION VARCHAR(320) NOT NULL,START DATETIME NOT NULL,FINISH DATETIME NOT NULL);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS EVENTS(E_ID INTEGER PRIMARY KEY AUTOINCREMENT,DESCRIPTION VARCHAR(320) NOT NULL,LOCATION VARCHAR(320) NOT NULL,DATE VARCHAR(10) NOT NULL UNIQUE,START VARCHAR(10) NOT NULL, FINISH VARCHAR(10) NOT NULL,FAVOURITE VARCHAR(10) NOT NULL);");
     }
 
     @Override
@@ -25,14 +25,23 @@ public class EventDatabase extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void insert(String description,String location,String start,String finish)
+    public void insert(String description,String location,String date,String start,String finish,String isFavourite)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("DESCRIPTION",description);
         contentValues.put("LOCATION",location);
+        contentValues.put("DATE",date);
         contentValues.put("START",start);
         contentValues.put("FINISH",finish);
+        contentValues.put("FAVOURITE",isFavourite);
         db.insert("EVENTS",null,contentValues);
+    }
+
+    public void populate()
+    {
+        insert("April Cleanup","Beach House","04/04/2020","10:00","12:00","No");
+        insert("May Cleanup","North Hut","03/05/2020","10:00","12:00","No");
+        insert("June Cleanup","South Hut","06/06/2020","10:00","12:00","No");
     }
 }
