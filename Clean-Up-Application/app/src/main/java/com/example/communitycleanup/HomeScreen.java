05d39d55cc4e359f13e3e6080f6456703e74041c
Calum Mortimer SD2019 <ekb19183@muir-11.cis.strath.ac.uk>
2020-03-11 14:43:16 +0000

    Added Basic Event Activity

--- Clean-Up-Application/app/src/main/java/com/example/communitycleanup/HomeScreen.java
+++ Clean-Up-Application/app/src/main/java/com/example/communitycleanup/HomeScreen.java
@@ -3,6 +3,8 @@ package com.example.communitycleanup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.auth.FirebaseUser;
+import android.content.Intent;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
     FirebaseUser user;
@@ -11,5 +13,11 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void events(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),EventsActivity.class);
+        startActivity(intent);
     }
 }
