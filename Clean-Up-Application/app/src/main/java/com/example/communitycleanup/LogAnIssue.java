d764677764103e6c722374ef55eebcc33b4dba1f
Tomas <51713488+TomDank@users.noreply.github.com>
2020-03-16 13:05:05 +0000

    Create LogAnIssue.java

---
+++ Clean-Up-Application/app/src/main/java/com/example/communitycleanup/LogAnIssue.java
@@ -0,0 +1,29 @@
+package com.example.communitycleanup;
+
+public class LogAnIssue {
+    public String postcode;
+    public String description;
+    //public String userEmail;
+
+    public LogAnIssue(String p, String d){
+        this.postcode = p;
+        this.description = d;
+        //this.userEmail = userEmail;
+    }
+
+    //void postCode(String p){
+      //  this.postcode = p;
+    //}
+
+    //void IssueDescription(String d){
+     //   this.description = d;
+    //}
+
+    //void submit(String p, String d){
+     //   this.postcode = p;
+      //  this.description = d;
+   // }
+
+
+
+}
