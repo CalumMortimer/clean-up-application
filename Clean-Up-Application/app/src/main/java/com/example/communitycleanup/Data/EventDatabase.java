79dc621ca566ebe1a0de1e5b7cfaa6a5294aeb2b
Calum Mortimer <calum.mortimer@btinternet.com>
2020-03-16 15:17:38 +0000

    Comments added to EventDatabase

--- Clean-Up-Application/app/src/main/java/com/example/communitycleanup/Data/EventDatabase.java
+++ Clean-Up-Application/app/src/main/java/com/example/communitycleanup/Data/EventDatabase.java
@@ -5,13 +5,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import com.example.communitycleanup.DataTransfer.Event;
-
 import java.util.ArrayList;
 
+/**An EventDatabase class which manages an SQLite database for the Events table
+ *
+ */
 public class EventDatabase extends SQLiteOpenHelper {
 
+    /**Create database object - discarding any events table which currently exists
+     * and creating a new Events table
+     */
     public EventDatabase(Context context)
     {
         super(context,"events.db",null,1);
@@ -20,17 +24,29 @@ public class EventDatabase extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    /**Create the table events, if it does not already exist*/
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS EVENTS(E_ID INTEGER PRIMARY KEY AUTOINCREMENT,DESCRIPTION VARCHAR(320) NOT NULL,LOCATION VARCHAR(320) NOT NULL,DATE VARCHAR(10) NOT NULL UNIQUE,START VARCHAR(10) NOT NULL, FINISH VARCHAR(10) NOT NULL,FAVOURITE VARCHAR(10) NOT NULL);");
     }
 
+    /**Drop the Events table and call onCreate on the database
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db,int a,int b) {
         db.execSQL("DROP TABLE IF EXISTS EVENTS");
         onCreate(db);
     }
 
+    /**Insert a row into the event table
+     *
+     * @param description a description of the event
+     * @param location a location of the event
+     * @param date a date the event takes place
+     * @param start the start time of the event
+     * @param finish the finish time of the event
+     * @param isFavourite whether the event is a favourite event or not
+     */
     public void insert(String description,String location,String date,String start,String finish,String isFavourite)
     {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -44,6 +60,8 @@ public class EventDatabase extends SQLiteOpenHelper {
         db.insert("EVENTS",null,contentValues);
     }
 
+    /**fills the events table with initial data
+     */
     public void populate()
     {
         insert("April Cleanup","Beach House","04/04/20","10:00","12:00","No");
@@ -51,6 +69,10 @@ public class EventDatabase extends SQLiteOpenHelper {
         insert("June Cleanup","South Hut","06/06/20","10:00","12:00","No");
     }
 
+    /**returns an ArrayList of all Event objects represented on the Events table
+     *
+     * @return ArrayList of Event objects
+     */
     public ArrayList<Event> getEventsList() {
         ArrayList<Event> fullList = new ArrayList<Event>();
         Event thisEvent = null;
